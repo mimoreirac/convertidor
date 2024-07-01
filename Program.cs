8c2242997c770dbc0bbbda3581745bb983e7fc83
Martin Moreira <172557112+mimoreirac@users.noreply.github.com>
2024-06-30 20:58:11 -0500

    Add metric converters

--- Program.cs
+++ Program.cs
@@ -14,3 +14,41 @@ Console.WriteLine("5. Centimetros.");
 Console.WriteLine("6. Milimetros.");
 Console.WriteLine("7. Millas.");
 Console.WriteLine("8. Pulgadas.");
+Console.WriteLine("9. Salir.");
+
+string opcion = Console.ReadLine();
+bool repetir = true;
+int resultado = 0;
+
+do
+{
+    switch (opcion)
+    {
+        case "1":
+            resultado = x / 1000;
+            Console.WriteLine($"{x} m son {resultado} km.");
+            break;
+        case "2":
+            resultado = x / 100;
+            Console.WriteLine($"{x} m son {resultado} hm.");
+            break;
+        case "3":
+            resultado = x / 10;
+            Console.WriteLine($"{x} m son {resultado} dm.");
+            break;
+        case "4":
+            resultado = x * 10;
+            Console.WriteLine($"{x} m son {resultado} dcm.");
+            break;
+        case "5":
+            resultado = x * 100;
+            Console.WriteLine($"{x} m son {resultado} cm.");
+            break;
+        case "6":
+            resultado = x * 1000;
+            Console.WriteLine($"{x} m son {resultado} mm.");
+            break;
+        default:
+            break;
+    }
+} while (repetir);
