aab4d9222e53cc970c14e57ec2d2026bd38e6fe5
Martin Moreira <172557112+mimoreirac@users.noreply.github.com>
2024-06-30 20:27:22 -0500

    First commit

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("~~~~~~~~~~~~ Convertidor de Unidades ~~~~~~~~~~~~~~~");
+Console.WriteLine("Bienvenido a su convertidor de unidades.");
+Console.WriteLine("Ingrese un valor en metros.");
+
+int x = int.Parse(Console.ReadLine());
+
+Console.WriteLine("A que unidad desea convertir?");
+Console.WriteLine("1. Kilometros.");
+Console.WriteLine("2. Hectometros.");
+Console.WriteLine("3. Decametros.");
+Console.WriteLine("4. Decimetros.");
+Console.WriteLine("5. Centimetros.");
+Console.WriteLine("6. Milimetros.");
+Console.WriteLine("7. Millas.");
+Console.WriteLine("8. Pulgadas.");
